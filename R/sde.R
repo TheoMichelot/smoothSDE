a81d3b1c02fb9521fea2774c29581dd9e618c0c9
Theo Michelot <theo.michelot@gmail.com>
2020-06-28 11:04:02 -0600

    make_mats method for SDE class

--- R/sde.R
+++ R/sde.R
@@ -38,9 +38,63 @@ SDE <- R6Class(
         ###################
         ## Other methods ##
         ###################
-        #' @description Make model matrices
-        make_mats() = function() {
-            return(NULL)
+        #' @description Create model matrices
+        #'
+        #' @param new_data Optional new data set, including covariates for which
+        #' the design matrices should be created.
+        #' 
+        #' @return A list of
+        #' \itemize{
+        #'   \item X_fe Design matrix for fixed effects
+        #'   \item X_re Design matrix for random effects
+        #'   \item S Smoothness matrix
+        #'   \item ncol_re Number of columns of X_re and S for each random effect
+        #' }
+        make_mats = function(new_data = NULL) {
+            # Initialise lists of matrices
+            X_list_fe <- list()
+            X_list_re <- list()
+            S_list <- list()
+            ncol_re <- NULL
+            k <- 1
+            
+            # Loop over formulas
+            for(form in self$formulas()) {
+                # Create matrices based on this formula
+                if(is.null(new_data)) {
+                    gam_setup <- gam(formula = update(form, dummy ~ .), 
+                                     data = cbind(dummy = 1, self$data()), 
+                                     fit = FALSE)
+                    Xmat <- gam_setup$X
+                } else {
+                    # Get design matrix for new data set
+                    gam_setup <- gam(formula = update(form, dummy ~ .), 
+                                     data = cbind(dummy = 1, self$data()))
+                    Xmat <- predict(gam_setup, newdata = new_data, type = "lpmatrix")
+                }
+                
+                # Fixed effects design matrix
+                X_list_fe[[k]] <- Xmat[, 1:gam_setup$nsdf, drop = FALSE]
+                
+                # Random effects design matrix
+                X_list_re[[k]] <- Xmat[, -(1:gam_setup$nsdf), drop = FALSE]
+                
+                # Smoothing matrix
+                S_list[[k]] <- bdiag_check(gam_setup$S)
+                
+                # Number of columns for each random effect
+                if(length(gam_setup$S) > 0)
+                    ncol_re <- c(ncol_re, sapply(gam_setup$S, ncol))
+                
+                k <- k + 1
+            }
+            
+            # Store as block diagonal matrices
+            X_fe <- bdiag_check(X_list_fe)
+            X_re <- bdiag_check(X_list_re)
+            S <- bdiag_check(S_list)
+            
+            return(list(X_fe = X_fe, X_re = X_re, S = S, ncol_re = ncol_re))
         },
         
         #' @description Setup model
