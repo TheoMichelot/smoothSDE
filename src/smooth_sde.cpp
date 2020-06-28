ae5ad48182f08d96f871a2744f7dc9f3f754b694
Theo Michelot <theo.michelot@gmail.com>
2020-06-28 12:11:12 -0600

    first go at objective function

--- src/smooth_sde.cpp
+++ src/smooth_sde.cpp
@@ -1,5 +1,10 @@
 
+#include "tr_dens.hpp"
+
+#ifndef _TMB_
+#define _TMB_
 #include <TMB.hpp>
+#endif
 
 template<class Type>
 Type objective_function<Type>::operator() ()
@@ -8,10 +13,83 @@ Type objective_function<Type>::operator() ()
     using namespace density; 
     using namespace Eigen; 
     
-    DATA_MATRIX(Z);
-    PARAMETER(beta);
+    //======//
+    // DATA //
+    //======//
+    DATA_VECTOR(ID); // Time series ID
+    DATA_VECTOR(times); // Observation times
+    DATA_MATRIX(Z); // Response variables
+    DATA_SPARSE_MATRIX(X_fe); // Design matrix for fixed effects
+    DATA_SPARSE_MATRIX(X_re); // Design matrix for random effects
+    DATA_SPARSE_MATRIX(S); // Penalty matrix
+    DATA_IVECTOR(ncol_re); // Number of columns of S and X_re for each random effect
+    DATA_IVECTOR(type); // Model type for each response variable
     
-    Type nllk = 0;
+    // Number of observations
+    int n = Z.rows();
+    // Time intervals
+    vector<Type> dtimes = diff(times);
+    
+    //============//
+    // PARAMETERS //
+    //============//
+    PARAMETER_VECTOR(coeff_fe); // Fixed effect parameters
+    PARAMETER_VECTOR(coeff_re); // Random effect parameters
+    PARAMETER_VECTOR(log_lambda); // Smoothness parameters
+    
+    // Derived parameters (linear predictors)
+    vector<Type> par_vec = X_fe * coeff_fe + X_re * coeff_re;
+    matrix<Type> par_mat(n, par_vec.size()/n);
+    for(int i = 0; i < par_mat.cols(); i++) {
+        // Matrix with one row for each time step and
+        // one column for each parameter
+        par_mat.col(i) = par_vec.segment(i*n, n);
+    }
+    
+    //============//
+    // Likelihood //
+    //============//
+    // Initialise log-likelihood
+    Type llk = 0;
+    // Loop over observations
+    for(int i = 1; i < n; i ++) {
+        // No contribution if first observation of the track
+        if(ID(i-1) == ID(i)) {
+            llk = llk + tr_dens<Type>(Z(i, 0), Z(i-1, 0), dtimes(i-1), 
+                                      par_mat.row(i-1), true, type(0));
+        }
+    }
+    
+    //===================//
+    // Smoothing penalty //
+    // ===================//
+    Type nllk = -llk;
+    // Are there random effects?
+    if(ncol_re(0) > 0) {
+        // Index in matrix S
+        int S_start = 0;
+        
+        // Loop over smooths
+        for(int i = 0; i < ncol_re.size(); i++) {
+            // Size of penalty matrix for this smooth
+            int Sn = ncol_re(i);
+            
+            // Penalty matrix for this smooth
+            Eigen::SparseMatrix<Type> this_S = S.block(S_start, S_start, Sn, Sn);
+            
+            // Coefficients for this smooth
+            vector<Type> this_coeff_re = coeff_re.segment(S_start, Sn);
+            
+            // Add penalty
+            nllk = nllk -
+                Type(0.5) * Sn * log_lambda(i) +
+                Type(0.5) * exp(log_lambda(i)) * 
+                density::GMRF(this_S).Quadform(this_coeff_re);
+            
+            // Increase index
+            S_start = S_start + Sn;
+        }
+    }
     
     return nllk;
 }
