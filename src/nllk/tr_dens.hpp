f4af69dda8deff4ab5bf848bf0f2296c21d0c8f3
Théo Michelot <tm75@st-andrews.ac.uk>
2020-12-02 15:33:55 +0000

    allow for multivariate obs in tr_dens for BM and BM-t

--- src/nllk/tr_dens.hpp
+++ src/nllk/tr_dens.hpp
@@ -12,30 +12,28 @@ Type tr_dens(vector<Type> Z1, vector<Type> Z0, Type dtimes, vector<Type> par,
     Type mean = 0;
     Type sd = 1;
     
-    if(type == "BM") {
-        // Brownian motion: dZ_t = mu(t) dt + sigma(t) dW_t
-        // where par = (mu, log(sigma))
+    // Number of dimensions
+    int n_dim = Z1.size();
+    
+    // Loop over dimensions to add contribution of each
+    for(int i = 0; i < n_dim; i++) {
         
-        mean = Z0(0) + par(0) * dtimes;
-        sd = exp(par(1)) * sqrt(dtimes);
-        res = dnorm(Z1(0), mean, sd, true);
-    } else if(type == "BM-t") {
-        // Brownian motion with t-distributed noise
-        
-        Type df = other_data(0); // number of degrees of freedom
-        mean = par(0) * dtimes;
-        sd = exp(par(1)) * sqrt(dtimes);
-        Type scale = sd/sqrt(df/(df-2));
-        res = dt((Z1(0)-Z0(0)-mean)/scale, df, true) - log(scale);
-    } else if(type == "OU") {
-        // Ornstein-Uhlenbeck: dZ_t = beta(t) (mu(t) - Z_t) dt + sigma(t) dW_t
-        // where par = mu_1, mu_2, ..., mu_d, log(beta), log(sigma)
-        
-        // Number of dimensions
-        int n_dim = Z1.size();
-        
-        // Loop over dimensions to add contribution of each
-        for(int i = 0; i < n_dim; i++) {
+        if(type == "BM") {
+            // Brownian motion: dZ_t = mu(t) dt + sigma(t) dW_t
+            // where par = (mu, log(sigma))
+            mean = Z0(i) + par(0) * dtimes;
+            sd = exp(par(1)) * sqrt(dtimes);
+            res = res + dnorm(Z1(i), mean, sd, true);
+        } else if(type == "BM-t") {
+            // Brownian motion with t-distributed noise
+            Type df = other_data(0); // number of degrees of freedom
+            mean = par(0) * dtimes;
+            sd = exp(par(1)) * sqrt(dtimes);
+            Type scale = sd/sqrt(df/(df-2));
+            res = res + dt((Z1(i)-Z0(i)-mean)/scale, df, true) - log(scale);
+        } else if(type == "OU") {
+            // Ornstein-Uhlenbeck: dZ_t = beta(t) (mu(t) - Z_t) dt + sigma(t) dW_t
+            // where par = mu_1, mu_2, ..., mu_d, log(beta), log(sigma)
             mean = par(i) + exp(- exp(par(n_dim)) * dtimes) * (Z0(i) - par(i));
             sd = exp(par(n_dim+1))/sqrt(2 * exp(par(n_dim))) * 
                 sqrt(1 - exp(-2 * exp(par(n_dim)) * dtimes));
