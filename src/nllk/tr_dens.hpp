0d301b8e3e291271f6b58dfaa55a063447a54090
Théo Michelot <tm75@st-andrews.ac.uk>
2020-12-16 10:12:27 +0000

    allow for NAs in BM/BM-t/OU models

--- src/nllk/tr_dens.hpp
+++ src/nllk/tr_dens.hpp
@@ -27,26 +27,29 @@ Type tr_dens(vector<Type> Z1, vector<Type> Z0, Type dtimes, vector<Type> par,
     
     // Loop over dimensions to add contribution of each
     for(int i = 0; i < n_dim; i++) {
-        if(type == "BM") {
-            // Brownian motion: dZ_t = mu(t) dt + sigma(t) dW_t
-            // where par = (mu, log(sigma))
-            mean = Z0(i) + par(0) * dtimes;
-            sd = exp(par(1)) * sqrt(dtimes);
-            res = res + dnorm(Z1(i), mean, sd, true);
-        } else if(type == "BM-t") {
-            // Brownian motion with t-distributed noise
-            Type df = other_data(0); // number of degrees of freedom
-            mean = par(0) * dtimes;
-            sd = exp(par(1)) * sqrt(dtimes);
-            Type scale = sd/sqrt(df/(df-2));
-            res = res + dt((Z1(i)-Z0(i)-mean)/scale, df, true) - log(scale);
-        } else if(type == "OU") {
-            // Ornstein-Uhlenbeck: dZ_t = beta(t) (mu(t) - Z_t) dt + sigma(t) dW_t
-            // where par = mu_1, mu_2, ..., mu_d, log(beta), log(sigma)
-            mean = par(i) + exp(- exp(par(n_dim)) * dtimes) * (Z0(i) - par(i));
-            sd = exp(par(n_dim+1))/sqrt(2 * exp(par(n_dim))) * 
-                sqrt(1 - exp(-2 * exp(par(n_dim)) * dtimes));
-            res = res + dnorm(Z1(i), mean, sd, true);
+        // No contribution if either start or end observation is missing
+        if(!R_IsNA(asDouble(Z0(i))) && !R_IsNA(asDouble(Z1(i)))) {
+            if(type == "BM") {
+                // Brownian motion: dZ_t = mu(t) dt + sigma(t) dW_t
+                // where par = (mu, log(sigma))
+                mean = Z0(i) + par(0) * dtimes;
+                sd = exp(par(1)) * sqrt(dtimes);
+                res = res + dnorm(Z1(i), mean, sd, true);
+            } else if(type == "BM-t") {
+                // Brownian motion with t-distributed noise
+                Type df = other_data(0); // number of degrees of freedom
+                mean = par(0) * dtimes;
+                sd = exp(par(1)) * sqrt(dtimes);
+                Type scale = sd/sqrt(df/(df-2));
+                res = res + dt((Z1(i)-Z0(i)-mean)/scale, df, true) - log(scale);
+            } else if(type == "OU") {
+                // Ornstein-Uhlenbeck: dZ_t = beta(t) (mu(t) - Z_t) dt + sigma(t) dW_t
+                // where par = mu_1, mu_2, ..., mu_d, log(beta), log(sigma)
+                mean = par(i) + exp(- exp(par(n_dim)) * dtimes) * (Z0(i) - par(i));
+                sd = exp(par(n_dim+1))/sqrt(2 * exp(par(n_dim))) * 
+                    sqrt(1 - exp(-2 * exp(par(n_dim)) * dtimes));
+                res = res + dnorm(Z1(i), mean, sd, true);
+            }            
         }
     }
     
