acbdf5f9312de5a69cb5562df1ddc3b60577c4c6
Théo Michelot <tm75@st-andrews.ac.uk>
2021-01-22 14:31:16 +0000

    add documentation for package itself (i.e., ?smoothSDE)

---
+++ R/smoothSDE-package.R
@@ -0,0 +1,7 @@
+
+#' @details
+#' The package smoothSDE is based on the class \link{SDE}. Type ?SDE 
+#' to find its documentation, or consult the package vignette for detailed 
+#' examples of the smoothSDE workflow.
+#' @keywords internal
+"_PACKAGE"
